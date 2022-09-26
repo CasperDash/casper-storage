761200d90dce435225c0b6cd57061569b1f821d4
Codsay <hoangdh91@gmail.com>
2022-09-27 00:09:21 +0700

    Remove key-encoder, use directly asn1 package

--- src/cryptography/key/parser/secp256k1-key-parser.ts
+++ src/cryptography/key/parser/secp256k1-key-parser.ts
@@ -1,9 +1,18 @@
 import { BaseKeyParser } from "./base-key-parser";
-import KeyEncoder from "key-encoder";
-import { TypeUtils } from "../../../utils";
 import { EncryptionType } from "../../../cryptography";
 
-const keyEncoder = new KeyEncoder('secp256k1');
+import * as asn1Js from "asn1.js";
+
+const ECPrivateKeyASN = asn1Js.define("ECPrivateKey", function () {
+  // @ts-ignore
+  const self = this as any
+  self.seq().obj(
+      self.key("version").int(),
+      self.key("privateKey").octstr(),
+      self.key("parameters").explicit(0).objid().optional(),
+      self.key("publicKey").explicit(1).bitstr().optional()
+  )
+})
 
 /**
  * Specific implementation of Secp256k1 to parse keys
@@ -19,8 +28,9 @@ export class Secp256k1KeyParser extends BaseKeyParser {
   }
 
   private parseKey(data: Uint8Array): Uint8Array {
-    const key = keyEncoder.encodePrivate(TypeUtils.convertArrayToHexString(data), 'der', 'raw'); // Convert digital certificate format to raw format
-    return TypeUtils.convertHexStringToArray(key);
+    // Convert digital certificate format to raw format
+    const privateKeyObject = ECPrivateKeyASN.decode(Buffer.from(data), "der");
+    return new Uint8Array(privateKeyObject.privateKey);
   }
-
 }
+
