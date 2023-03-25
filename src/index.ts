f1b69a9a8cdf7095b8246a536feab4f20acaba22
hoang <hoangdh91@gmail.com>
2023-03-25 11:26:26 +0000

    Cycle upgrade dependencies

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,3 @@ export * from "./wallet";
 export * from "./user";
 export * from "./storage";
 export * from "./bips/bip32/hdkey/core";
-
-// Replace the native implementaion of sha512 inside ed25519
-// by the one from hashes, which can be polyfilled by rn-nodeify
-import * as ed from "@noble/ed25519";
-import { sha512 } from "@noble/hashes/sha512";
-ed.utils.sha512 = (msg) => Promise.resolve(sha512(msg))
